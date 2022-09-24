0a4dc8d60d1ca81baac055c77ef218dadf45a888
yukinoandhui <1659056318@qq.com>
2022-09-24 17:01:33 +0800

    add function

--- course-case/chapter2.cpp
+++ course-case/chapter2.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
+#include<stdio.h>
 using namespace std;
-
+typedef unsigned char *pointer;
 /*
  *这里开始都是函数的定义
  * */
@@ -11,11 +12,18 @@ int uadd_ok(unsigned x,unsigned y){
     return 1;
     }
     return 0;
-
-
 }
 
+//数据在内存中的表示
+void show_bytes(pointer start,size_t len){
+    size_t i;
+    for (i=0;i<len;i++){
+        printf("%p\t0x%.2x\n",start+i,start[i]);
+    }
+}
 int main(){
-    cout<<uadd_ok(32,32)<<endl;
+    //cout<<uadd_ok(32,32)<<endl;
+    int a=15213;
+    show_bytes((pointer)&a,sizeof(int));
     return 0;
 }
