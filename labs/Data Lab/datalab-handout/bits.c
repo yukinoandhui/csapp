844cfa0614d4482d2723e3d6e9abb3cda53c6257
yukinoandhui <1659056318@qq.com>
2022-09-25 08:15:47 +0800

    nothing

--- labs/Data Lab/datalab-handout/bits.c
+++ labs/Data Lab/datalab-handout/bits.c
@@ -143,7 +143,7 @@ NOTES:
  *   Rating: 1
  */
 int bitXor(int x, int y) {
-  return 2;
+  return ;
 }
 /* 
  * tmin - return minimum two's complement integer 
@@ -152,8 +152,8 @@ int bitXor(int x, int y) {
  *   Rating: 1
  */
 int tmin(void) {
-
-  return 2;
+    
+  return ;
 
 }
 //2
